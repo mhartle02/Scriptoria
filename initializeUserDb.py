c772f1b609e33397d0c0fa3f2cc3ce889f4711fe
Bryce Beck <bjb22b@fsu.edu>
2025-02-08 16:08:34 -0500

    Haven't fully finished testing the code but here's what I have for now

---
+++ initializeUserDb.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+def initializeUserDatabase():
+    conn = sqlite3.connect('userLoginDatabase.db')
+    cursor = conn.cursor()
+    #cursor.execute("DROP TABLE IF EXISTS userLogins")
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS userLogins (
+        id INTEGER PRIMARY KEY AUTOINCREMENT
+        name TEXT NOT NULL
+        username TEXT NOT NULL
+        password TEXT NOT NULL
+        permission TEXT NOT NULL
+        )
+    ''')
+
+    #Temporary, for debugging
+    exampleUsers = [
+        {"name": "John", "username": "Reader", "password": "Password", "permission": "reader"},
+        {"name": "John", "username": "Admin", "password": "Password", "permission": "admin"},
+        {"name": "John", "username": "Author", "password": "Password", "permission": "author"}
+    ]
+
+    for user in exampleUsers:
+        cursor.execute('''
+                INSERT INTO userLogins (name, username, password, permission)
+                VALUES (?, ?, ?, ?)
+            ''', (user["name"], user["username"], user["password"], user["permission"]))
+    conn.commit()
+
+    cursor.execute("SELECT name, username, password, permission FROM userLogins")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(f"UserId: {row[0]}, Name: {row[1]}, Username: {row[2]}, Password: {row[3]}, Permission: {row[4]}")
+    conn.close()
+
+
+    if __name__ == "__main__":
+        initializeUserDatabase()

540e163a0f2914ef1419a84fb9d8afe167905e4c
Mason Hartle <mhartle02@gmail.com>
2025-03-18 18:46:32 -0400

    some changes

--- app.py
+++ app.py
@@ -1,61 +1,18 @@
 from flask import *
 import sqlite3, requests
 from werkzeug.security import generate_password_hash, check_password_hash
+from libraries import GoogleBooks
 
 app = Flask(__name__)
 app.secret_key = 'Secret Key'
-#We should open a connection right before we use it and close right after
-#I feel like if we do it enclosing like this then we'll close the db, and it won't get reopened for a new page
-#conn = sqlite3.connect('Scriptoria.db')
-#cursor = conn.cursor()
-
 app.secret_key = "keys" #needed to change the password
-
-class Book:
-    def __init__(self, book_id, title, authors, description, page_count, cover_image, published_date, reviews, buy_link):
-        self.book_id = book_id
-        self.title = title
-        self.authors = authors
-        self.description = description
-        self.page_count = page_count
-        self.cover_image = cover_image
-        self.published_date = published_date
-        #self.reviews = reviews     #Need to return to
-        self.buy_link = buy_link
-    def __str__(self):
-        return f"Title: {self.title}\nAuthors: {', '.join(self.authors)}\nPublished: {self.published_date}\nMore Info: {self.buy_link}\n"
-
-def fetch_books(query, max_results=5):
-    #Arbitrary max_results number, for testing
-    url = f"https://www.googleapis.com/books/v1/volumes?q={query}&maxResults={max_results}"
-    response = requests.get(url)
-    data = response.json()
-
-    books = []
-    for item in data.get('items', []):
-        volume_info = item.get('volumeInfo', {})
-        book = Book(
-            book_id=volume_info.get('id', 'Unknown ID'),
-            title=volume_info.get('title', 'Unknown Title'),
-            authors=volume_info.get('authors', ['Unknown Author']),
-            description=volume_info.get('description', 'No Description'),
-            page_count=volume_info.get('pageCount', 0),
-            published_date=volume_info.get('publishedDate', 'Unknown Date'),
-            cover_image=volume_info.get('imageLinks', {}).get('thumbnail', ''),
-            buy_link=item.get('saleInfo', {}).get('buyLink', '')
-        )
-        books.append(book)
-    return books
-
 @app.route('/')
-def home():  # put application's code here
-    #try:
-        #test = cursor.execute("select USERNAME from users WHERE password = 'password'").fetchone()
-        #print(test)
-        #return test[0]
-    #except:
-        #return 'Database connection failed'
-    return render_template('home.html')     #Honestly idk if 'home.html' here is correct, but this should be the landing page
+def home():
+
+    query = request.args.get('q', '')
+    print(type(GoogleBooks.search_books(query)))
+
+    return render_template('home.html')
 
 @app.route('/login', methods =['GET', 'POST'])
 def login():
--- libraries/GoogleBooks.py
+++ libraries/GoogleBooks.py
@@ -1,4 +1,5 @@
 import requests
+from classes import book
 
 API_KEY = "AIzaSyDHIFBhhb6fnzBS0-GcXap4bh5IIbF-KnI"
 BASE_URL = "https://www.googleapis.com/books/v1/volumes"
@@ -17,16 +18,17 @@ def search_books(query, max_results=5):
         data = response.json()
         books = data.get("items", [])  # Extract book list
 
-        formatted_results = []
-        for book in books:
-            volume_info = book.get("volumeInfo", {})
-            title = volume_info.get("title", "No title available")
-            authors = volume_info.get("authors", ["Unknown author"])
-            description = volume_info.get("description", "No description available")
 
-            formatted_results.append(f"Title: {title}\nAuthor: {', '.join(authors)}\nDescription: {volume_info.get('description')}")
+        results = []
 
-        return "\n\n".join(formatted_results)  # Join results with spacing
+        for item in books:
+            volume_info = item.get("volumeInfo", {})
+            book_result= book.book(title = volume_info.get("title", "No title available", authors = volume_info.get("authors", ["Unknown author"]),
+                                                       description = volume_info.get("description", "No description available")))
+
+            results.append(book_result)
+
+        return results  # Join results with spacing
     else:
         return f"Error {response.status_code}: {response.text}"
 
@@ -34,3 +36,4 @@ def search_books(query, max_results=5):
 if __name__ == "__main__":
     books_info = search_books("East of Eden") #Pass in book name
     print(books_info)
+

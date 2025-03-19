ae1bfc7f99513cc955c7bff3dd52079aee288f0d
Mason Hartle <mhartle02@gmail.com>
2025-03-18 23:17:20 -0400

    Update GoogleBooks.py

--- libraries/GoogleBooks.py
+++ libraries/GoogleBooks.py
@@ -1,4 +1,10 @@
 import requests
+import sys
+import os
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(path)
+from classes import book
+
 
 API_KEY = "AIzaSyDHIFBhhb6fnzBS0-GcXap4bh5IIbF-KnI"
 BASE_URL = "https://www.googleapis.com/books/v1/volumes"
@@ -17,20 +23,21 @@ def search_books(query, max_results=5):
         data = response.json()
         books = data.get("items", [])  # Extract book list
 
-        formatted_results = []
-        for book in books:
-            volume_info = book.get("volumeInfo", {})
-            title = volume_info.get("title", "No title available")
-            authors = volume_info.get("authors", ["Unknown author"])
-            description = volume_info.get("description", "No description available")
 
-            formatted_results.append(f"Title: {title}\nAuthor: {', '.join(authors)}\nDescription: {volume_info.get('description')}")
+        results = []
 
-        return "\n\n".join(formatted_results)  # Join results with spacing
+        for item in books:
+            volume_info = item.get("volumeInfo", {})
+            book_result= book.book(volume_info.get("title", ["Unknown Title"]), volume_info.get("authors", ["Unknown author"]),volume_info.get("description", "No description available"))
+
+            results.append(book_result)
+
+        return results  # Join results with spacing
     else:
         return f"Error {response.status_code}: {response.text}"
 
 
 if __name__ == "__main__":
-    books_info = search_books("East of Eden") #Pass in book name
-    print(books_info)
+    #books_info = search_books("East of Eden") #Pass in book name
+    #print(books_info[0].title)
+

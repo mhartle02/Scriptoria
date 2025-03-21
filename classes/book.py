ed4586d2c67dccba1a10227948732648fcb97b94
Bryce Beck <bjb22b@fsu.edu>
2025-03-21 12:59:09 -0400

    Added ReviewDB and BookDB and got BookDb working

--- classes/book.py
+++ classes/book.py
@@ -1,10 +1,14 @@
 class Book:
     def __init__(self, title, authors, description, page_count, cover_image):
         self.title = title
-        self.authors = authors
+        #self.authors = "".join(authors) if isinstance(authors, list) else authors
+        self.authors = ", ".join(authors)
         self.description = description
         self.page_count = page_count
         self.cover_image = cover_image
 
+        #More debugging for author comma issue
+        #print(f"Processed Authors: {self.authors}")
+
     def __str__(self):
-        return f"Title: {self.title}\nAuthors: {', '.join(self.authors)}\nDescription: {self.description}\n"
+        return f"Title: {self.title}\nAuthors: {self.authors}\nDescription: {self.description}\n"
